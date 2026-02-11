c57e5be355cb3ab18df1381e675f7157e1b145d3
vtelesp <valtellesp@hotmail.com>
2026-02-11 20:55:20 -0300

    First commit, enviando primeiro exercicio e adicionando arquivos essenciais.

---
+++ src/fundamentos/variaveis/printVariables.java
@@ -0,0 +1,16 @@
+package fundamentos.variaveis;
+
+public class printVariables {
+    public static void main(String[] args) {
+
+        String nome = "Ichigo";
+        int idade = 17;
+        double altura = 1.85;
+        boolean isEmployed = false;
+
+        System.out.println("Nome do personagem: " + nome);
+        System.out.println("A idade dele é: " + idade);
+        System.out.println("Sua altura é de: " + altura);
+        System.out.println("Está empregado?: " + isEmployed);
+    }
+}
